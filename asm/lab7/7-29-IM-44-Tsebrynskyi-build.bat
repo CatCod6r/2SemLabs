4337591dec32f42290fbd8cd6e44806b23f7d751
CatCod6r <ncebrinskij@gmail.com>
2026-05-15 16:47:17 +0300

    lab7 asm, lab 4,6 physics

--- asm/lab7/7-29-IM-44-Tsebrynskyi-build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-\masm32\bin\ml /c /coff 7-29-IM-44-Tsebrynskyi-main.asm
-\masm32\bin\ml /c /coff 7-29-IM-44-Tsebrynskyi-proc.asm
-\masm32\bin\link /SUBSYSTEM:WINDOWS 7-29-IM-44-Tsebrynskyi-main.obj 7-29-IM-44-Tsebrynskyi-proc.obj
-pause
